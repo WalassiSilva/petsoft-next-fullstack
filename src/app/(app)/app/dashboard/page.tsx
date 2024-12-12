62633affeb803382916552e79af1de7e6679f136
Walassi Silva <walassi.b@gmail.com>
2024-12-12 19:37:10 -0300

    feat: add `branding` and `stats` sections

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+import BrandingSection from '@/components/branding-section'
+import StatsSection from '@/components/stats-section'
 import React from 'react'
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <BrandingSection />
+        <StatsSection />
+      </div>
+    </main>
   )
 }
---
+++ src/components/branding-section.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import H1 from './h1'
+
+export default function BrandingSection() {
+  return (
+    <section>
+      <H1>Pet<span className='font-semibold leading-6'>Soft</span></H1>
+      <p className='opacity-80 text-lg'>Manage your pet daycare with ease</p>
+    </section>
+  )
+}
