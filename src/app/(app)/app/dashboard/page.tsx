2af7b9896373df5fb83e8fced774311036f7d810
Walassi Silva <walassi.b@gmail.com>
2024-12-13 19:43:14 -0300

    feat: add `dashboard page`

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,14 +1,35 @@
-import BrandingSection from '@/components/branding-section'
-import StatsSection from '@/components/stats-section'
-import React from 'react'
+import BrandingSection from "@/components/branding-section";
+import PetDetails from "@/components/pet-details";
+import PetList from "@/components/pet-list";
+import SearchForm from "@/components/search-form";
+import StatsSection from "@/components/stats-section";
+import React from "react";
 
 export default function DashboardPage() {
   return (
     <main>
-      <div className="flex items-center justify-between text-white py-8">
+      <div className="flex items-center justify-between text-white py-8 ">
         <BrandingSection />
         <StatsSection />
       </div>
+
+      <div className="grid grid-rows-[45px_300px_500px] md:grid-cols-3 md:grid-rows-[45px_1fr] gap-4 md:h-[600px]">
+        <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1 ">
+          <SearchForm />
+        </div>
+
+        <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
+          <div className="block-container">
+            <PetList />
+          </div>
+        </div>
+
+        <div className="md:col-span-2 md:row-span-full">
+          <div className="block-container">
+            <PetDetails />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
